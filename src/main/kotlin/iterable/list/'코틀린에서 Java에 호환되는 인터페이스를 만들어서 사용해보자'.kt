cbe645c0dab2fbe784007be40420fbcc55f0c1bc
Choi Sang Il <wo566@naver.com>
2022-10-25 00:52:59 +0900

    study: 어렵다!

--- src/main/kotlin/iterable/list/'코틀린에서 Java에 호환되는 인터페이스를 만들어서 사용해보자'.kt
+++ src/main/kotlin/iterable/list/'코틀린에서 Java에 호환되는 인터페이스를 만들어서 사용해보자'.kt
@@ -1,2 +1,15 @@
+
 package iterable.list
 
+import java.util.*
+
+fun main(){
+    val americano = Coffee("americano", 5000)
+    val mix = Coffee("mix", 300)
+    val espresso = Coffee("espresso", 4000)
+
+    val list: List<Coffee> = listOf(americano)
+    println(list)
+}
+
+public fun <T> listOf(element: T): List<T> = Collections.singletonList(element)
