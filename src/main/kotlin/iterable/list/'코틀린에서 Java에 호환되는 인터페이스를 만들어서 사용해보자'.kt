041d6786c810ae123c19a71f2e0c04b1c64ffd07
Choi Sang Il <wo566@naver.com>
2022-10-26 23:36:56 +0900

    study: 이펙티브 코틀린 스터디

--- src/main/kotlin/iterable/list/'코틀린에서 Java에 호환되는 인터페이스를 만들어서 사용해보자'.kt
+++ src/main/kotlin/iterable/list/'코틀린에서 Java에 호환되는 인터페이스를 만들어서 사용해보자'.kt
@@ -8,8 +8,23 @@ fun main(){
     val mix = Coffee("mix", 300)
     val espresso = Coffee("espresso", 4000)
 
-    val list: List<Coffee> = listOf(americano)
+    val list: List<Coffee> = listOf(americano, mix)
     println(list)
+
+    val list2 : List<Coffee> = java.util.ArrayList<Coffee>()
+
+
+    if(list2 is java.util.List<*>){
+        println("test")
+    }
+
+    if(list2 is List<*>){
+        //list2.add(espresso)
+        println("test ${list2}")
+    }
+
+    //list2.add(mix)
+    println(list2)
 }
 
 public fun <T> listOf(element: T): List<T> = Collections.singletonList(element)
