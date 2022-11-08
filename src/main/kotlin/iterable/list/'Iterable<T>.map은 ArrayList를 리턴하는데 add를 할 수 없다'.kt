0861700fb17be5b4fc7ee2e2cef746ba16ceb9bb
Choi Sang Il <wo566@naver.com>
2022-11-08 13:04:54 +0900

    study: 이펙티크 코틀린 study..

--- src/main/kotlin/iterable/list/'Iterable<T>.map은 ArrayList를 리턴하는데 add를 할 수 없다'.kt
+++ src/main/kotlin/iterable/list/'Iterable<T>.map은 ArrayList를 리턴하는데 add를 할 수 없다'.kt
@@ -2,11 +2,13 @@ package iterable.list
 
 fun main() {
     val cities = listOf("Seoul","Tokyo","MountainView")
-    println(cities.javaClass) //class java.util.Arrays$ArrayList
+    println(cities.javaClass)   //class java.util.Arrays$ArrayList
+
     val list = cities.map { str: String -> str.toUpperCase() }
-    println(list.javaClass) //class java.util.ArrayList
+    println(list.javaClass)     //class java.util.ArrayList
     //list.add("a") 오류
-    val test = mutableListOf<String>("a", "b", "c")
-    println(test.javaClass)
-    test.add("d")
+
+    val mutableList = mutableListOf<String>("a", "b", "c")
+    println(mutableList.javaClass)     //class java.util.ArrayList
+    mutableList.add("d")
 }
